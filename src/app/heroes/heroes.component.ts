b33322ce53157a0b6db1b87976975027e16b53a7
petert360 <peter.t360@hotmail.com>
2021-02-15 21:26:24 +0100

    HeroComponent update.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Hero } from '../model/hero';
+import { HeroService } from '../service/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  heroList$: BehaviorSubject<Hero[]> = this.heroService.list$;
+
+  constructor(
+    private heroService: HeroService,
+  ) { }
 
   ngOnInit(): void {
+    this.heroService.getAll();
   }
 
 }
