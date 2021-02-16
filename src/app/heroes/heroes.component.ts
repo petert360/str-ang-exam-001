2bb6366549c9b99e0578fbd0674084c4f3c834c6
petert360 <peter.t360@hotmail.com>
2021-02-16 11:48:57 +0100

    Update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,16 +11,13 @@ export class HeroesComponent implements OnInit {
 
   heroList: Hero[] = this.heroService.getAll();
   phrase: string = '';
-
   columnKey: string = '';
 
   constructor(
     private heroService: HeroService,
   ) { }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
 
   onChangePhrase(event: Event): void {
     this.phrase = (event.target as HTMLInputElement).value;
